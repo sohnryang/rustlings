00167f741206bace9e3243423a05941417ac2d79
Ryang Sohn <loop.infinitely@gmail.com>
2021-11-27 16:28:48 +0900

    Finish chapter standard_library_types

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,23 +1,17 @@
 // iterators4.rs
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return factorial of num
-    // Do not use:
-    // - return
-    // For extra fun don't use:
-    // - imperative style loops (for, while)
-    // - additional variables
-    // For the most fun don't use:
-    // - recursion
-    // Execute `rustlings hint iterators4` for hints.
+    (1..=num).fold(1, |acc, x| acc * x)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[test]
+    fn factorial_of_0() {
+        assert_eq!(1, factorial(0));
+    }
     #[test]
     fn factorial_of_1() {
         assert_eq!(1, factorial(1));
